76df57bd59ec024daa23918324cc727b77a1884c
sheeplegs430 <A!pplerocks2>
2017-05-11 20:53:31 -0700

    Adding second activty and goto button.

--- app/src/main/java/ucsc/cmps121/ucscparking/MainActivity.java
+++ app/src/main/java/ucsc/cmps121/ucscparking/MainActivity.java
@@ -1,13 +1,28 @@
 package ucsc.cmps121.ucscparking;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+{
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
+
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+
+    public void goMainMenu(View v)
+    {
+        Intent intent = new Intent(this, MainMenu.class);
+        startActivity(intent);
+    }
+
 }
+
