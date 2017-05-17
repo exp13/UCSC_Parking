458bb8136e7a450acb7b812f02388221669a7852
exp13 <switkin@ucsc.edu>
2017-05-16 17:38:20 -0700

    updated second activity/mainmenu and added accountprefs activity

--- app/src/main/java/ucsc/cmps121/ucscparking/AccountPrefs.java
+++ app/src/main/java/ucsc/cmps121/ucscparking/AccountPrefs.java
@@ -5,16 +5,13 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
-public class MainMenu extends AppCompatActivity {
+public class AccountPrefs extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main_menu);
+        setContentView(R.layout.activity_account_prefs);
     }
 
-    public void goAccountPrefs(View v){
-        Intent intent = new Intent(this, AccountPrefs.class);
-        startActivity(intent);
-    }
+
 }
