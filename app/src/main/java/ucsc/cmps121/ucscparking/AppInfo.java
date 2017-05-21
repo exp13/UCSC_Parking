f0e988486735290a8575ec61c4a5b718e5b0aad5
exp13 <switkin@ucsc.edu>
2017-05-20 18:52:21 -0700

    misc edits

--- app/src/main/java/ucsc/cmps121/ucscparking/AppInfo.java
+++ app/src/main/java/ucsc/cmps121/ucscparking/AppInfo.java
@@ -3,6 +3,7 @@ package ucsc.cmps121.ucscparking;
 import android.content.Context;
 import android.content.SharedPreferences;
 
+
 /**
  * Created by luca on 18/1/2016.
  */
@@ -10,46 +11,29 @@ public class AppInfo {
 
     private static AppInfo instance = null;
 
-
-    // Here are some values we want to keep global.
-    public String sharedString;
-
-    // static private member for storing strings
-    private String[] sharedStrings;
-
     private Context my_context;
 
     protected AppInfo() {
-        // initialise empty string array
-        sharedStrings = new String[3];
-        sharedStrings[0] = "";
-        sharedStrings[1] = "";
-        sharedStrings[2] = "";
-    }
 
+    }
 
     public static AppInfo getInstance(Context context) {
         if(instance == null) {
             instance = new AppInfo();
             instance.my_context = context;
-            /*SharedPreferences settings = context.getSharedPreferences(MainActivity.MYPREFs, 0);
-            instance.sharedStrings[0] = settings.getString("one", null);
-            instance.sharedStrings[1] = settings.getString("two", null);
-            instance.sharedStrings[2] = settings.getString("three", null);*/
+            SharedPreferences settings = context.getSharedPreferences("UCSCParking", 0);
         }
         return instance;
     }
 
 
-    // get string method for shared strings
-    public String getString(int index){
-        return instance.sharedStrings[index];
-    }
+
 
     // set string method for shared strings
-    public void setString(String s, int index){
+    // saving this for example code
+    /*public void setString(String s, int index){
         instance.sharedStrings[index] = s;
-        /*SharedPreferences settings = my_context.getSharedPreferences(MainActivity.MYPREFS, 0);
+        SharedPreferences settings = my_context.getSharedPreferences(MainActivity.MYPREFS, 0);
         SharedPreferences.Editor editor = settings.edit();
         switch(index){
             case 0:
@@ -62,6 +46,6 @@ public class AppInfo {
                 editor.putString("three", s);
                 break;
         }
-        editor.commit();*/
-    }
+        editor.commit();
+    }*/
 }
