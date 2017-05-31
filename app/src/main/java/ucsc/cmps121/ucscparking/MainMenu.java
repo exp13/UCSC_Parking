2d797867b1f57df4a7cb38d28ea9c1d3c98060e2
exp13 <switkin@ucsc.edu>
2017-05-30 21:14:39 -0700

    added backend module, test code for datastore using objectify API

--- app/src/main/java/ucsc/cmps121/ucscparking/MainMenu.java
+++ app/src/main/java/ucsc/cmps121/ucscparking/MainMenu.java
@@ -1,9 +1,28 @@
 package ucsc.cmps121.ucscparking;
 
+import android.content.Context;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Pair;
 import android.view.View;
+import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.net.ssl.HttpsURLConnection;
 
 public class MainMenu extends AppCompatActivity {
 
@@ -11,6 +30,8 @@ public class MainMenu extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
+
+        new ServletPostAsyncTask().execute(new Pair<Context, String>(this, "Manfred"));
     }
 
     public void goAccountPrefs(View v){
@@ -26,5 +47,79 @@ public class MainMenu extends AppCompatActivity {
     public void goCamera(View v) {
         Intent intent = new Intent(this, CameraActivity.class);
         startActivity(intent);
+    }
+
+    class ServletPostAsyncTask extends AsyncTask<Pair<Context, String>, Void, String> {
+        private Context context;
+
+        @Override
+        protected String doInBackground(Pair<Context, String>... params) {
+            context = params[0].first;
+            String name = params[0].second;
+
+            try {
+                // Set up the request
+                URL url = new URL("http://ucscparking-1.appspot.com/hello");
+                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+                connection.setRequestMethod("POST");
+                connection.setDoInput(true);
+                connection.setDoOutput(true);
+
+                // Build name data request params
+                Map<String, String> nameValuePairs = new HashMap<>();
+                nameValuePairs.put("name", name);
+                String postParams = buildPostDataString(nameValuePairs);
+
+                // Execute HTTP Post
+                OutputStream outputStream = connection.getOutputStream();
+                BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(outputStream, "UTF-8"));
+                writer.write(postParams);
+                writer.flush();
+                writer.close();
+                outputStream.close();
+                connection.connect();
+
+                // Read response
+                int responseCode = connection.getResponseCode();
+                StringBuilder response = new StringBuilder();
+                BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    response.append(line);
+                }
+                reader.close();
+
+                if (responseCode == HttpsURLConnection.HTTP_OK) {
+                    return response.toString();
+                }
+                return "Error: " + responseCode + " " + connection.getResponseMessage();
+
+            } catch (IOException e) {
+                return e.getMessage();
+            }
+        }
+
+        private String buildPostDataString(Map<String, String> params) throws UnsupportedEncodingException {
+            StringBuilder result = new StringBuilder();
+            boolean first = true;
+            for (Map.Entry<String, String> entry : params.entrySet()) {
+                if (first) {
+                    first = false;
+                } else {
+                    result.append("&");
+                }
+
+                result.append(URLEncoder.encode(entry.getKey(), "UTF-8"));
+                result.append("=");
+                result.append(URLEncoder.encode(entry.getValue(), "UTF-8"));
+            }
+
+            return result.toString();
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            Toast.makeText(context, result, Toast.LENGTH_LONG).show();
+        }
     }
 }
